4f3182ef14368b3ab1b852842715ca424f368f26
DominicSamoes <dominicsamo@outlook.com>
2022-03-11 07:54:07 -0800

    0x00 Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,8 +17,9 @@ int main(void)
 			{
 				putchar((num1 % 10) + '0');
 				putchar((num2 % 10) + '0');
+				putchar((num3 % 10) + '0');
 
-				if (num1 == 8 && num2 == 9)
+				if (num1 == 7 && num2 == 8 && num3 == 9)
 					break;
 
 				putchar(',');
