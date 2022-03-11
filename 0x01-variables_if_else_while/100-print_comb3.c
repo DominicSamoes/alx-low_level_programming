117c0af613ceb3fac56ce98c179248732930192b
DominicSamoes <dominicsamo@outlook.com>
2022-03-11 04:00:01 -0800

    0x01 Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ *main - Prints all the possible combinations of of two digits.
+ *
+ * Return: 0 always.
+ */
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		for (num2 = 0; num2 < 10; num2++)
+		{
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 9 && num2 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -8,3 +8,4 @@ Task 6, C program that prints all single digit numbers of base 10 starting from 
 Task 7, C program prints the lowercase alphabet in reverse, followed by a new line.
 Task 8, C program prints all the numbers of base 16 in lowercase, followed by a new line.
 Task 9, C program that prints all possible combinations of single-digit numbers.
+Task 10, C program  that prints all possible different combinations of two digits.
