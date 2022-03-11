b1584aa275c6c2b81df2ca1b12c92fbac6491197
DominicSamoes <dominicsamo@outlook.com>
2022-03-11 04:12:37 -0800

    0x01 Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,17 +9,18 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 < 10; num1++)
+	for (num1 = 48; num1 <= 57; num1++)
 	{
-		for (num2 = 1; num2 < 10; num2++)
+		for (num2 = 48; num2 <= 57; num2++)
 		{
-			putchar((num1 % 10) + '0');
-			putchar((num2 % 10) + '0');
+			putchar(num1);
+			putchar(num2;
 
-			if (num1 == 9 && num2 == 9)
-				continue;
-			putchar(',');
-			putchar(' ');
+			if (num1 != 57 || num2 != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 
