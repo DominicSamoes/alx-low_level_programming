0a558f1bc45e488d194c32c7045646423b12a668
DominicSamoes <dominicsamo@outlook.com>
2022-03-15 06:06:32 -0700

    0x02 Task 2 Correction

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * print_aphabet_x10 - Prints the alphabet in lowercase 10 times, followed by a new line
+ * print_alphabet_x10 - Prints the alphabet in lowercase 10.
  */
 void print_alphabet_x10(void)
 {
-        char alpha;
+	char alpha;
 	int counter;
 
 	counter = 0;
