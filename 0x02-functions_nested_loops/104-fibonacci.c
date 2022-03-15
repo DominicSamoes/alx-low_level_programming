223adc16fb6eba51871f37d0fd015991e957668f
DominicSamoes <dominicsamo@outlook.com>
2022-03-15 08:55:10 -0700

    0x02 Task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,54 @@
+i/*
+ * Auth: Dominic Samo
+ */
+
+#include <stdio.h>
+
+/**
+ *main - Prints first 50 Fibonacci numbers.
+ *
+ *Return: 0 always.
+ */
+int main(void)
+{
+	int i;
+	unsigned long fibo1 = 0, fibo2 = 1, sum;
+	unsigned long fibo1_h1, fibo1_h2, fibo2_h1, fibo2_h2;
+	unsigned long h1, h2;
+
+	for (i = 0; i <= 91; i++)
+	{
+		sum = fibo1 + fibo2;
+		printf("%lu, ", sum);
+
+		fibo1 = fibo2;
+		fibo2 = sum;
+	}
+	fibo1_h1 = fibo1 / 10000000000;
+	fibo2_h1 = fibo2 / 10000000000;
+	fibo1_h2 = fib1 % 10000000000;
+	fibo2_h2 = fibo2 % 10000000000;
+
+	for (i = 93; i < 99; i++)
+	{
+		h1 = fibo1_h1 + fibo2_h1;
+		h2 = fibo1_h2 + fibo2_h2;
+		if (fibo1_h2 + fibo2_h2 > 10000000000 - 1)
+		{
+			h1 += 1;
+			h2 %= 10000000000;
+		}
+
+		printf("%lu%lu", h1, h2);
+		if (i != 98)
+			printf(", ");
+
+		fibo1_h1 = fibo2_h1;
+		fibo1_h2 = fibo2_h2;
+		fibo2_h1 = h1;
+		fibo2_h2 = h2;
+	}
+	printf("\n");
+
+	return (0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -12,4 +12,6 @@ Task 10, Function that adds two integers and returns the result.
 Task 11, Function that prints all natural numbers from n to 98, followed by a new line.
 Task 12, Function that prints the n times table, starting with 0.
 Task 13, Program that computes and prints the sum of all the multiples of 3 or 5 below 1024 (excluded), followed by a new line.
-Task 14, Program that prints the first 50 Fibonacci numbers, starting with 1 and 2, followed by a new line. 
+Task 14, Program that prints the first 50 Fibonacci numbers, starting with 1 and 2, followed by a new line.
+Task 15, Program that finds and prints the sum of the even-valued terms, followed by a new line
+Task 16, Program that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line. 
