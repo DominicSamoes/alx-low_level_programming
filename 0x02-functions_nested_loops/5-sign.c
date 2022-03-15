ef3e3332b5ae7cb507858cb53b959b39b1a60b49
DominicSamoes <dominicsamo@outlook.com>
2022-03-15 06:43:45 -0700

    0x02 Task 5 Correction

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * print_sign - Prints the sign of a number
+ * print_sign - Prints the sign of a number.
+ *
+ * @n: number whose sign is to be checked.
+ *
+ * Return: 1 when n>0, 0 when n=0 and -1 for n<0.
+ *
  */
 int print_sign(int n)
 {
-        if (n > 0)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
