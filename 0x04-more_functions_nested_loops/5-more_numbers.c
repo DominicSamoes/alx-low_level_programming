c27b235eaae118002ebfb4d584e54b702104c739
DominicSamoes <dominicsamo@outlook.com>
2022-03-17 12:51:21 +0200

    0x04 Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -18,7 +18,7 @@ void more_numbers(void)
 		for (n = 0; n < 15; n++)
 		{
 			if (n > 9)
-				_putchar((n % 10) + '0');
+				_putchar((n / 10) + '0');
 			_putchar((n % 10) + '0');
 		}
 		_putchar('\n');
