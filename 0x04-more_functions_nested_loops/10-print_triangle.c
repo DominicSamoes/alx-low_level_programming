32c60fdbebeb3086e968b69ea0d0a699cc8a2ad2
DominicSamoes <dominicsamo@outlook.com>
2022-03-17 15:13:11 +0200

    0x04 Task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -16,7 +16,7 @@ void print_triangle(int size)
 
 	if (size > 0)
 	{
-		for (i = 0; i <= size; i++)
+		for (i = 1; i <= size; i++)
 		{
 			for (index = size - i; index > 0; index--)
 				_putchar(' ');
