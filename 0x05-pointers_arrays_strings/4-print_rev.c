d879e352ec85d1a6996c678aff7c89f438ed7101
DominicSamoes <dominicsamo@outlook.com>
2022-03-21 16:44:26 +0200

    0x05 Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+/*
+ * File: 4-print_rev.c
+ * Author: Dominic Samo
+ */
+
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse.
+ *
+ * @s: pointer to char.
+ */
+void print_rev(char *s)
+{
+	int len, i;
+
+	len = 0;
+
+	while (s[i++])
+		i++;
+
+	for (i = len - 1; i >= 0; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
