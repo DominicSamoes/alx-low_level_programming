277e7482b78ccd05565b29490d8c40ae67607d61
DominicSamoes <dominicsamo@outlook.com>
2022-03-21 17:22:27 +0200

    0x05 Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+/*
+ * File: 8-print_array.c
+ * Author: Dominic Samo
+ */
+
+#include "main.h"
+
+/**
+ * print_array - Prints n elements of an array of ints
+ *	followed by a new line.
+ * @a: pointer to char.
+ * @n: number of elements to be printed.
+ */
+void print_array(char *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i == n - 1)
+			continue;
+
+		printf(", ");
+	}
+
+	printf("\n");
+}
