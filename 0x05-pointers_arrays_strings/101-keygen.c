58d98a0943934713aeea20e8c353ba23d3d0a330
DominicSamoes <dominicsamo@outlook.com>
2022-03-21 23:13:05 +0200

    0x05 Task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,40 @@
+/*
+ * File: 101-keygen.c
+ * Author: Dominic Samo
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Generates random valid passwords for
+ *	the program 101-crackme.
+ *
+ * Return: 0 always.
+ */
+int main(void)
+{
+	int code[100], i, sum, num;
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		code[i] = rand() % 78;
+		sum += (code[i] + '0');
+		putchar(code[i] + '0');
+
+		if ((2772 - sum) - '0' < 78)
+		{
+			num = 2772 - sum - '0';
+			sum += num;
+			putchar(num + '0');
+			break;
+		}
+	}
+
+	return (0);
+}
