1d53f938b9e7ddc819230ef35d9379ac7119b9c9
DominicSamoes <dominicsamo@outlook.com>
2022-03-24 14:16:51 +0200

    0x06 Task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -16,7 +16,7 @@ char *rot13(char *str)
 {
 	int i = 0, j;
 
-	char alph[32] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+	char alph[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
 		'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
 		'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b',
 		'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
