8f87d1cead54397bbafc3531a13238047fe0ef84
DominicSamoes <dominicsamo@outlook.com>
2022-03-30 15:29:05 +0200

    0x08 Task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+/*
+ *File: 1-print_rev_recursion.c
+ *Auth: Dominic Samo
+ */
+
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse.
+ *
+ * @s: string.
+ *
+ * Return: no return.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,1 +1,2 @@
 Task 1 - function that prints a string, followed by a new line.
+Task 2 - function prints a string in reverse.
