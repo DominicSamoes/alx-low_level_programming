cce8f24a2e199360b7d04fa0cd927b75ec70639e
DominicSamoes <dominicsamo@outlook.com>
2022-03-30 16:08:07 +0200

    0x08 Task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+/*
+ *File: 5-sqrt_recursion.c
+ *Auth: Dominic Samo
+ */
+
+#include "main.h"
+
+/**
+ * _sqrt_recursion - naturl square root of a number.
+ *
+ * @n: integer.
+ *
+ * Return: value of x^(1/2) else -1 if y < 0.
+ */
+int _sqrt_recursion(int n)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (n / _sqrt_recursion(n - 1));
+	}
+}
