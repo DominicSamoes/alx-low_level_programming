3cc2159b5f4eda4efd345838d10dc595be413ed2
DominicSamoes <dominicsamo@outlook.com>
2022-03-30 16:33:48 +0200

    0x08 Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,6 +6,32 @@
 #include "main.h"
 
 /**
+ * sqrt_rec - does bin search while computing sqrt.
+ *
+ * @st: initial integer.
+ * @en: last number
+ * @num: equivalent to n.
+ *
+ * Return: 1 if not found else return root.
+ */
+int sqrt_rec(int st, int en, int num)
+{
+	long midval;
+
+	if (en >= st)
+	{
+		midval = st + (en - st) / 2;
+		if (midval * midval == num)
+			return (midval);
+		if (midval * midval > num)
+			return (sqrt_rec(st, midval - 1, num));
+		if (midval * midval < num)
+			return (sqrt_rec(midval + 1, en, num));
+	}
+	return (-1);
+}
+
+/**
  * _sqrt_recursion - naturl square root of a number.
  *
  * @n: integer.
@@ -14,31 +40,14 @@
  */
 int _sqrt_recursion(int n)
 {
-	double temp = n/2;
-
 	if (n < 0)
 	{
 		return (-1);
 	}
-	else if (n == 0)
+	if (n == 0 || n == 1)
 	{
-		return (0);
+		return (n);
 	}
-	else
-	{
-		do 
-		{
-			if ((temp * temp) > n)
-			{
-				temp /= 2;
-			}
-			if (temp * temp < n)
-			{
-				temp += (n - temp);
-			}
 
-		} while (((n - temp) >= .001) || (temp - n) >= .001)
-
-		return (temp);
-	}
+	return (sqrt_rec(2, n, n));
 }
