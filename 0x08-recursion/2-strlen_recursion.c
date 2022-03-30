ebe2531b1238088cc6b2f6192c7a777361dbfee0
DominicSamoes <dominicsamo@outlook.com>
2022-03-30 15:33:41 +0200

    0x08 Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,25 @@
+/*
+ *File: 2-strlen_recursion.c
+ *Auth: Dominic Samo
+ */
+
+#include "main.h"
+
+/**
+ * _strlen_recursion - find length of string.
+ *
+ * @s: string.
+ *
+ * Return: length of string.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
