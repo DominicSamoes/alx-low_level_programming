2ca320c9760415a3a4dd219fb3223f454d4a82b0
DominicSamoes <dominicsamo@outlook.com>
2022-04-01 17:27:48 +0200

    0x09 Task 0 Correction 2

---
+++ 0x09-static_libraries/lib/3-puts.c
@@ -0,0 +1,22 @@
+/*
+ * File: 3-puts.c
+ * Author: Dominic Samo
+ */
+
+#include "main.h"
+
+/**
+ * _puts - Prints a string.
+ *
+ * @str: pointer to char.
+ */
+void _puts(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
