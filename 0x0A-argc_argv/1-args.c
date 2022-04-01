f8f13327219de386feb298c9507a2c30c664f357
DominicSamoes <dominicsamo@outlook.com>
2022-04-01 18:09:09 +0200

    0x0A Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - outputs the number of arguements passed to it
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ * Return: 0
+ **/
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argv - 1);
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,1 +1,2 @@
 Task 0: program that prints its name, followed by a new line.
+Task 1: program that prints the number of arguments passed into it.
