aa9b1d599150d434dd2a187e1ef515cee817dbb4
DominicSamoes <dominicsamoes@gmail.com>
2022-04-08 11:57:53 +0200

    0x0C Task 2 Correction

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,21 +16,21 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *pt;
+	char *p;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	pt = malloc(nmemb * size);
+	p = malloc(nmemb * size);
 
-	if (pt == NULL)
+	if (p == NULL)
 		return (NULL);
 
 	for (i = 0; i < (nmemb * size); i++)
-		pt[i] = 0;
+		p[i] = 0;
 
-	return (pt);
+	return (p);
 }
 
 
