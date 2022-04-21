aa20072329fe821533e3accfc6158733dab675af
DominicSamoes <dominicsamoes@gmail.com>
2022-04-21 13:46:30 +0200

    0x12 Task 2 Correction

--- 0x12-singly_linked_lists/testing/2-add_node.c
+++ 0x12-singly_linked_lists/testing/2-add_node.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <string.h>
 /**
  * add_node - adds a new node at the beginning
  * of a list_t list.
@@ -10,29 +11,19 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	char *dup;
-	int len = 0;
+	unsigned int len = 0;
 
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	dup = strdup(str);
-
-	if (dup == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
-	for (; str[len]; len++)
+	while (str[len])
 		len++;
 
-	new_node->str = dup;
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->str = strdup(str);
 	new_node->len = len;
-	new_node->next = *head;
+	new_node->next = (*head);
+	(*head) = new_node;
 
-	*head = new_node;
-
-	return (new_node);
+	return (*head);
 }
