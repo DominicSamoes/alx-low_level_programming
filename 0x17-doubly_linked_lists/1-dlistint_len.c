d69a4d0544b5d9f64a88376efa993a001da6e8a6
DominicSamoes <dominicsamo@outlook.com>
2022-06-09 22:23:46 +0200

    C 0x17 Task 1 Correction

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,20 +2,16 @@
 #include "lists.h"
 
 /**
-  * dlistint_len - Counts the number of elements in a doubly linked list
-  * @h: Doubly linked list to be counted
+  * dlistint_len - Count the number of elements in a DLL
+  * @h: Pointer to head of DLL
   *
-  * Return: Number of elements in doubly linked list
+  * Return: Number of elements in the DLL
   */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t nodes = 0;
+	int nodes = 0;
 
-	if (!h)
-		return (0);
-	while (h->prev)
-		h = h->prev;
-	while (h) 
+	while (h != NULL)
 	{
 		++nodes;
 		h = h->next;
